bd747f3f515902812e5cc3e7ea05009db6b26f5d
bean-liu <529519366@qq.com>
2021-12-28 16:49:56 +0800

    遗漏

--- src/components/library/SearchBar.vue
+++ src/components/library/SearchBar.vue
@@ -3,7 +3,7 @@
         <el-input  
         @keyup.enter.native="searchClick"
         placeholder="通过书名或作者搜索..."
-        prefix-icon="el-icon-seach"
+        prefix-icon="el-icon-search"
         size="small"
         style="width: 400px; margin-right: 10px"
         v-model="keywords"
